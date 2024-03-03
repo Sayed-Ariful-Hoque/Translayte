4ea746ae69e65f8ad1f324606ac4aa196226326a
Sayed-Ariful-Sayed <107043600+Sayed-Ariful-Sayed@users.noreply.github.com>
2024-03-03 09:02:08 +0600

    translayte

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    extend: {
+      spacing: {
+        container: '1440px',
+        container2: "1425px",
+      }
+    },
+  },
+  plugins: [],
+}
